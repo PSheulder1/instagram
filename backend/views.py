ab0ea69060a6b91ac751c0f619f69fdef90858cb
PSheulder1 <psheulder@gmail.com>
2025-06-03 07:35:03 -0700

    Update views.py

--- backend/views.py
+++ backend/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, redirect
 
 from django.contrib.auth.models import User
 
+from django.contrib.auth import authenticate, login as auth_login
+
 # Create your views here.
 
 
@@ -11,6 +13,16 @@ def home(request):
 
 
 def login(request):
+    if request.method=="POST":
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(request, username=username, password=password)
+        if user:
+            auth_login(request, user)
+            return redirect('home')
+        else:
+            return render(request, 'login.html', {"error": "Invalid credentials"})
     return render(request, 'login.html')
 
 
